2d5e0df83cd8d79b119eaa678dd720bdabf0d2ea
Manvir Singh <sehmbimanvir@gmail.com>
2020-01-04 17:52:34 +0530

    Show Select Box on Small Devices Instead of Navbar

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -3,20 +3,38 @@ import React from 'react'
 const Languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
 
 const Navbar = (props) => {
+
+  const onChangeSelect = (e) => {
+    props.onChange(e.target.value)
+  }
+
   return (
-    <nav className="custom-nav text-center">
-      <ul className="list-inline">
-        {
-          Languages.map((item, index) => {
-            return (
-              <li className='list-inline-item' key={index}>
-                <button className={`btn-clear ${props.language === item ? 'nav-active' : ''}`} onClick={() => props.onChange(item)}>{item}</button>
-              </li>
-            )
-          })
-        }
-      </ul>
-    </nav>
+    <>
+      <nav className="custom-nav text-center d-none d-md-block d-lg-none">
+        <ul className="list-inline">
+          {
+            Languages.map((item, index) => {
+              return (
+                <li className='list-inline-item' key={index}>
+                  <button className={`btn-clear ${props.language === item ? 'nav-active' : ''}`} onClick={() => props.onChange(item)}>{item}</button>
+                </li>
+              )
+            })
+          }
+        </ul>
+      </nav>
+      <div className="form-group language-selector d-block d-sm-block d-md-none mt-4">
+        <select onChange={onChangeSelect} className="form-control">
+          {
+            Languages.map((item, index) => {
+              return (
+                <option key={index} value={item}>{item}</option>
+              )
+            })
+          }
+        </select>
+      </div>
+    </>
   )
 }
 export default Navbar

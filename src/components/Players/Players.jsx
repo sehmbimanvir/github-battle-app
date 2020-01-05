d9c84dd83736efbc31049eb029aaef1e3c2fa9b2
Manvir Singh <sehmbimanvir@gmail.com>
2020-01-05 17:53:15 +0530

    Margin Added Between Player Cards in Mobile

--- src/components/Players/Players.jsx
+++ src/components/Players/Players.jsx
@@ -5,7 +5,7 @@ const Players = () => (
   <>
     <h1 className="text-center font-weight-light mt-4">Players</h1>
     <div className="row mt-4">
-      <div className="col-12 col-sm-12 col-md-6 col-xl-6">
+      <div className="col-12 col-sm-12 col-md-6 col-xl-6 mb-sm-4 mb-4 mb-md-0">
         <Player title="Player One" />
       </div>
       <div className="col-12 col-sm-12 col-md-6 col-xl-6">

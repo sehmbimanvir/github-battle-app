df86e5e9ad8022aff4ba8d6b03fb70ca2b6a918b
Manvir <sehmbimanvir@gmail.com>
2020-01-23 00:48:12 +0530

    Converted to Functional Component (Hooks Used)

--- src/containers/Result.jsx
+++ src/containers/Result.jsx
@@ -1,80 +1,77 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import queryString from 'query-string'
 import { getUser } from '../services/Github'
 import ResultCard from '../components/ResultCard/ResultCard'
 import Loader from '../components/UI/Loader'
 
-class Result extends Component {
+const Result = ({ location, history }) => {
+  const [players, updatePlayers] = useState([])
 
-  constructor (props) {
-    super(props)
-    this.state = {
-      players: []
-    }
-  }
+  const setPlayerDetails = ({ playerOne: user1, playerTwo: user2 }) => {
+    Promise.all([getUser(user1), getUser(user2)]).then(response => {
 
-  componentDidMount () {
-    document.title = this.props.title
-    const urlParams = queryString.parse(this.props.location.search)
-    this.setPlayerDetails(urlParams.playerOne)
-    this.setPlayerDetails(urlParams.playerTwo)
+      const [user1Details, user2Details] = response.map(item => {
+        const data = item.data
+        data.score = data.followers + data.public_repos
+        return data
+      })
 
-  }
-
-  setPlayerDetails = username => {
-    getUser(username).then(response => {
-      setTimeout(() => {
-        const result = response.data
-        const players = [...this.state.players]
-        result.score = result.followers + result.public_repos
-        players.push(response.data)
-        this.setState({ players })
-      }, 1500)
+      updatePlayers([...players, user1Details, user2Details])
     })
   }
 
-  resetBattle = () => {
-    this.props.history.push('/battle')
+  const resetBattle = () => {
+    history.push('/battle')
   }
 
-  render () {
-    if (this.state.players.length === 2) {
-      let [player1, player2] = this.state.players
-      const winner = player2.score > player1.score
+  const ResultCardWrapper = ({ player1, player2 }) => (
+    <div className="row">
+      <div className="col-12 col-sm-6 col-md-6 col-lg-5 mb-4">
+        <ResultCard winner item={player1} />
+      </div>
 
-      /** Shift Winner to First Place */
-      if (winner) {
-        [player2, player1] = [player1, player2]
-      }
+      <div className="col-12 col-sm-6 col-md-6 col-lg-5 offset-lg-2">
+        <ResultCard item={player2} />
+      </div>
+    </div>
+  )
 
-      return (
-        <>
-          <div className="row mt-5">
-            <div className="col-12 col-md-8 col-lg-8 offset-lg-2 offset-md-2">
-              <div className="row">
-                <div className="col-12 col-sm-6 col-md-6 col-lg-5 mb-4">
-                  <ResultCard winner item={player1} />
-                </div>
-                {/* <div className="col-2"></div> */}
-                <div className="col-12 col-sm-6 col-md-6 col-lg-5 offset-lg-2">
-                  <ResultCard item={player2} />
-                </div>
-              </div>
-            </div>
+  const ResetBattleBtn = () => (
+    <div className="row mt-5 mb-4">
+      <div className="col-md-2 col-12 offset-md-5 text-center">
+        <button onClick={resetBattle} className="btn-block btn btn-dark">Reset</button>
+      </div>
+    </div>
+  )
 
-          </div>
-          <div className="row mt-5 mb-4">
-            <div className="col-md-2 col-12 offset-md-5 text-center">
-              <button onClick={this.resetBattle} className="btn-block btn btn-dark">Reset</button>
-            </div>
-          </div>
-        </>
-      )
+  useEffect(() => {
+    const urlParams = queryString.parse(location.search)
+    setPlayerDetails(urlParams)
+  }, [])
+
+  if (players.length === 2) {
+    let [player1, player2] = players
+    const winner = player2.score > player1.score
+
+    /** Shift Winner to First Place */
+    if (winner) {
+      [player2, player1] = [player1, player2]
     }
-    return (<div className="mt-5 text-center">
-      <Loader text='Battling' />
-    </div>)
+
+    return (
+      <>
+        <div className="row mt-5">
+          <div className="col-12 col-md-8 col-lg-8 offset-lg-2 offset-md-2">
+            <ResultCardWrapper player1={player1} player2={player2} />
+          </div>
+        </div>
+        <ResetBattleBtn />
+      </>
+    )
   }
+  return (<div className="mt-5 text-center">
+    <Loader text='Battling' />
+  </div>)
 }
 
 export default Result
